2b3e856ebbdb1e6e715cacaa0bfb2b0b54fc2091
Rahul <rahulor2000@gmail.com>
2021-03-04 19:07:05 +0530

    finished not for mobile

--- script/script.js
+++ script/script.js
@@ -13,7 +13,14 @@ const options = [
     }
 ]
 
-const showOptions = document.getElementById("options");
+let PLAY = false
+let SELECTED = false
+let FINISH = false
+let myMark = 0;
+let aiMark = 0;
+const showOptions = document.getElementById("options")
+const yourScore = document.getElementById("your-score")
+const aiScore = document.getElementById("ai-score")
 
 options.map(item => {
     const { id, img } = item;
@@ -24,12 +31,28 @@ options.map(item => {
     </div>
     `
 })
-document.getElementById("me").src = 'https://semantic-ui.com/images/wireframe/image.png';
-document.getElementById("ai").src = 'https://semantic-ui.com/images/wireframe/image.png';
-let SELECTED = false
-const selected = (id) => {
+
+const play = () => {
+    PLAY = true
+}
+
+const reset = () => {
+    document.getElementById("me").src = 'https://semantic-ui.com/images/wireframe/image.png';
     document.getElementById("ai").src = 'https://semantic-ui.com/images/wireframe/image.png';
-    if (!SELECTED) {
+    document.getElementById("resultImage").removeAttribute("src");
+    SELECTED = false
+}
+const selected = (id) => {
+    if (myMark === 5 || aiMark === 5) {
+        PLAY = false
+        FINISH = true
+        if (myMark === 5) {
+            document.getElementById("result").innerHTML = `<h1>ooohh</h1>`
+        } else if (aiMark === 5) {
+            document.getElementById("result").innerHTML = `<h1>yaaah</h1>`
+        }
+    }
+    if (!SELECTED && !FINISH && PLAY) {
         SELECTED = true
         const item = options.find(item => item.id === id)
         document.getElementById("me").src = item.img
@@ -37,17 +60,18 @@ const selected = (id) => {
     }
 }
 
+
+
 const ai = (playerId) => {
+    document.getElementById("resultImage").src = '../img/oohman2.png'
     setTimeout(() => {
-        SELECTED = false
         let { id, img } = options[random()];
         document.getElementById("ai").src = img
         mark(playerId, id)
     }, 2000)
 
     setTimeout(() => {
-        document.getElementById("me").src = 'https://semantic-ui.com/images/wireframe/image.png';
-        document.getElementById("ai").src = 'https://semantic-ui.com/images/wireframe/image.png';
+        reset();
     }, 3500)
 }
 
@@ -60,18 +84,20 @@ const mark = (playerId, id) => {
     if (playerId === id) {
         return showStatus(0)
     } else if (playerId === 'rock' && id === 'csissors' || playerId === 'paper' && id === 'rock' || playerId === 'csissors' && id === 'paper') {
+        yourScore.innerText = `${myMark += 1}/5`
         return showStatus(1)
     } else if (playerId === 'csissors' && id === 'rock' || playerId === 'rock' && id === 'paper' || playerId === 'paper' && id === 'csissors') {
+        aiScore.innerText = `${aiMark += 1}/5`
         return showStatus(-1)
     }
 }
 
 const showStatus = (mark) => {
     if (mark == 0) {
-        console.log("drow");
-    } else if (mark == -1) {
-        console.log("fail");
+        document.getElementById("resultImage").src = '../img/non.jpeg';
     } else if (mark == 1) {
-        console.log("won");
+        document.getElementById("resultImage").src = '../img/hhuuu.png';
+    } else if (mark == -1) {
+        document.getElementById("resultImage").src = '../img/happy-imogi.jpg';
     }
 }
